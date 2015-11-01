7165e8b396622e8b97e728f0f3a14d33c71778cb
Ulrich Hoffmann <uho@xlerb.de>
2015-11-01 13:52:04 +0100

    Define more arithmetic words

--- arithmetic.fs
+++ arithmetic.fs
@@ -4,6 +4,17 @@
 : 1+ ( x1 -- x2 ) 1 + ;
 : 2+ ( x1 -- x2 ) 2 + ;
 : 1- ( x1 -- x1 ) 1 - ;
+: 0<> ( x -- f ) - 0= ;
+: 0> ( n1 -- f )  0 > ;
+: <> ( x1 x2 -- f ) = 0= ;
+: NEGATE ( n1 -- n2 ) 0 SWAP - ;
 
 : MIN ( n1 n2 -- n3 )  OVER OVER > IF SWAP THEN DROP ;
 : MAX ( n1 n2 -- n3 )  OVER OVER < IF SWAP THEN DROP ;
+: ABS ( n -- u )  DUP 0< IF NEGATE THEN ;
+
+: */ ( n1 n2 n3 -- n4 )  */MOD SWAP DROP ;
+: /MOD ( n1 n2 -- n3 ) 1 SWAP */MOD ;
+
+primitive U<
+: WITHIN ( n1|u1 n2|u2 n3|u3 -- flag )  OVER - >R - R> U< ;
